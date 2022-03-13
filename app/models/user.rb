cef3f744f68ac97a2cb9940c1a93b238eba50aa6
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-13 19:07:36 -0400

    add user validation test and new user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, :last_name, :password, :password_confirmation, :email, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 6}
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    subject do
+      described_class.new(first_name: 'Bob', last_name: 'Dylan', email: 'maggiesfarm@nomore.com', password: 'biabhs', password_confirmation: 'biabhs')
+    end
+
+    it 'should be valid if user has a name, email, password, and password confirmation' do
+      expect(subject).to be_valid
+    end
+
+    it 'should be invalid if no password is provided' do
+      subject.password = nil
+      expect(subject).to be_invalid
+
+    end
+    it 'should be invalid if password confirmation does not match' do
+      subject.password_confirmation = 'biabsss'
+      expect(subject).to be_invalid
+    end
+    it 'should be invalid if no first name is provided' do
+      subject.first_name = nil
+      expect(subject).to be_invalid
+    end
+    it 'should be invalid if no last name is provided' do
+      subject.last_name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'should be invalid if email already exists' do
+      subject.save
+
+      already_exists = User.new(first_name: 'Bobby', last_name: 'Dylaney', email: 'MaggiesFARM@nomore.COM', password: 'biabhs', password_confirmation: 'biabhs')
+      expect(already_exists).to be_invalid
+    end
+
+    it 'should be invalid if password is less than 6 characters in length' do
+      subject.password = "biabh"
+      subject.password_confirmation = "biabh"
+      expect(subject).to be_invalid
+    end
+
+  end
+end
