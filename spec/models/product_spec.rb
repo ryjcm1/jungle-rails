4f1060b95c84bdcf247a468da8ae68a2e829c9c3
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-13 17:19:15 -0400

    add product spec model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do 
+    # validation tests/examples here
+    it ('should have a name') do
+    end
+    
+    it('should have a price') do 
+    end
+
+    it('should have a quantity') do
+
+    end
+    it ('should have a category') do 
+    end 
+    
+  end
+
+end
