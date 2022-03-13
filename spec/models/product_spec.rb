e40ae49788d5843eebd6d2ce0da04d34e6a082eb
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-13 18:07:35 -0400

    add test for admin products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,18 +3,61 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do 
     # validation tests/examples here
-    it ('should have a name') do
+
+    it ('should save properly with all four fields set') do
+      @category = Category.new
+      @product = @category.products.new
+      @product.name = "Harry Potter and the Philosopher's Stone"
+      @product.quantity = 12
+      @product.price = 13.99
+      @product.save
+      expect(@product).to be_valid
+
+    end
+
+    it ('should not be created without a name') do
+      @category = Category.new
+      @product = @category.products.new
+      @product.name = nil
+      @product.quantity = 12
+      @product.price = 13.99
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+
     end
     
-    it('should have a price') do 
+    it('should not be created without a price') do
+      @category = Category.new
+      @product = @category.products.new
+      @product.name = "Harry Potter and the Philosopher's Stone"
+      @product.quantity = 12
+      @product.price_cents = nil
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+
     end
 
-    it('should have a quantity') do
-
+    it('should not be created without a quantity') do
+      @category = Category.new
+      @product = @category.products.new
+      @product.name = "Harry Potter and the Philosopher's Stone"
+      @product.quantity = nil
+      @product.price = 13.99
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
-    it ('should have a category') do 
+
+    it ('should not be created without a category') do 
+      @category = Category.new
+      @product = @category.products.new
+      @product.name = "Harry Potter and the Philosopher's Stone"
+      @product.quantity = 24
+      @product.price = 13.99
+      @product.category = nil
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end 
-    
+
   end
 
 end
