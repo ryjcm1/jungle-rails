8510d500b712f9b6722f368f38c9133940e5fec0
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-13 23:54:43 -0400

    add user login feature spec

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "UserLogins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+  before :each do
+    #create user
+    @user = User.create!(
+      first_name: "Bon",
+      last_name: "Jovi",
+      email: "livingonaprayer@frank.com",
+      password: "ididitmyway",
+      password_confirmation: "ididitmyway"
+    )
+  end
+
+  
+  scenario "They be redirected to the homepage after successfully logining in" do
+    visit '/login'
+
+    
+    fill_in 'email', with: @user.email
+
+    fill_in 'password', with: @user.password
+
+    click_button 'Submit'
+
+    expect(page).to have_content('Logout')
+    
+    # save_screenshot
+
+  end
+
+  
 end
