d2bd7618dccab0082b79c5d0e113e51adae4f350
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-13 22:46:09 -0400

    setup add to cart feature spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They should see the number in cart number change from 0 to 1" do
+
+
+
+  end
+end
